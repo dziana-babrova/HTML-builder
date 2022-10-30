96579835686f195d941d677eb4b2ddeeeef90ace
Diana_Bobrova <dziana_babrova@epam.com>
2022-10-30 22:22:01 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,43 @@
+const fsPromise = require("fs/promises");
 const fs = require("fs");
 const path = require("node:path");
 const { stdout } = require("process");
 
-fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {
-  if (err) throw err;
-});
+createDir(__dirname, "files", "files-copy");
+copyDir(__dirname, "files", "files-copy");
 
-fs.readdir(path.join(__dirname, "files"), { withFileTypes: false }, (err, files) => {
-  if (err) throw new Error("Something went wrong");
-  else {
-    for (let i = 0; i < files.length; i++) {
-      try {
-          copyFile(files[i], path.join(__dirname, "files-copy", files[i]));
-          console.log('source.txt was copied to destination.txt');
-        } catch {
-          console.log('The file could not be copied');
-      }
-    };
+async function createDir(readDirectoryPath, readDirectoryName, copyName) {
+  await fsPromise.mkdir(path.join(readDirectoryPath, copyName), { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+}
+
+async function copyDir(readDirectoryPath, readDirectoryName, copyName) {
+
+  const filesCreated = await fsPromise.readdir(path.join(readDirectoryPath, copyName));
+
+  for (let i = 0; i < filesCreated.length; i++) {
+    const stats = await fsPromise.stat(path.join(readDirectoryPath, readDirectoryName, filesCreated[i]));
+    if (stats.isFile()) {
+      await fsPromise.unlink(path.join(readDirectoryPath, copyName, filesCreated[i]));
+    } else {
+      await fsPromise.rmdir(path.join(readDirectoryPath, copyName, filesCreated[i]));
+    }
   }
-});
+
+  const files = await fsPromise.readdir(path.join(readDirectoryPath, readDirectoryName));
+  for (let i = 0; i < files.length; i++) {
+    const stats = await fsPromise.stat(path.join(readDirectoryPath, readDirectoryName, files[i]));
+    if (stats.isDirectory()) {
+      await fsPromise.mkdir(path.resolve(__dirname, copyName, files[i]), { recursive: true }, (err) => {
+        if (err) throw err;
+      });
+    } else {
+      await fsPromise.copyFile(
+        path.join(readDirectoryPath, readDirectoryName, files[i]),
+        path.resolve(readDirectoryPath, copyName, files[i])
+      );
+    }
+  }
+  stdout.write("The files has been copied successfully");
+}
