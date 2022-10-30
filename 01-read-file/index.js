5dd81723b485001230ece6e3718671e03fae22fc
Diana_Bobrova <dziana_babrova@epam.com>
2022-10-30 23:59:43 +0300

    refactor: move path to variable for 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,6 +3,9 @@ const path = require("node:path");
 
 const { stdout } = process;
 
-const readableStream = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
+const pathToFile = path.join(__dirname, "text.txt");
+
+const readableStream = fs.createReadStream(pathToFile, "utf-8");
+
 readableStream.on("error", (error) => console.log("Error", error.message));
 readableStream.on("data", (data) => stdout.write(data));
