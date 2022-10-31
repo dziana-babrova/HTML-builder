7d383c77b3ab1e0a5577f53bb37ee3a21b937df0
Diana_Bobrova <dziana_babrova@epam.com>
2022-10-31 16:28:19 +0300

    refactor: move path to folder to variable in 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,16 +4,12 @@ const { stdout } = require("process");
 
 async function readDir() {
   try {
-    const pathToFolder = path.join(__dirname, "secret-folder");
-    const data = await fs.readdir(pathToFolder);
-
+    const data = await fs.readdir(path.join(__dirname, "secret-folder"));
     for (let i = 0; i < data.length; i++) {
-
-      const stats = await fs.stat(path.join(pathToFolder, data[i]));
+      const stats = await fs.stat(path.join(__dirname, "secret-folder", data[i]));
       if (stats.isFile()) {
         stdout.write(`${path.parse(data[i]).name} - ${path.parse(data[i]).ext.slice(1)} - ${stats.size / 1000}Kb\n`);
       }
-
     }
   } catch {
     throw new Error("Something went wrong");
